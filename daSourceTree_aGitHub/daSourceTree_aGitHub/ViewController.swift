7dd50a748627bbff3a3c1b0f9a50031bd3b0d664
Paolo Prinzivalli <p.prinzivalli@wakala.it>
2022-09-25 11:00:16 +0200

    Pubblicazione intero nuovo progetto

---
+++ daSourceTree_aGitHub/daSourceTree_aGitHub/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  daSourceTree_aGitHub
+//
+//  Created by Paolo Prinzivalli on 25/09/22.
+//
+
+// videotutorial: https://www.youtube.com/watch?v=sOW2NbAzciM
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
